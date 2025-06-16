e440d01cd512a7e814d4993fbd7e8966004b7318
AlonaCyberAce <alona@cyberace.ca>
2025-06-16 13:04:58 -0600

    cant host with gh pages because of the styled components

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,10 @@
-// const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
-// const nextConfig = {
-//     output: 'export',
-//     basePath: isGithubPages ? '/portfolio_next_styled' : '',
-//     assetPrefix: isGithubPages ? '/portfolio_next_styled/' : '',
-// };
-
-// export default nextConfig;
 
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
 const nextConfig = {
-    output: 'export', // tells Next to prepare for static export
-    basePath: isGithubPages ? '/portfolio_next_styled' : '',
-    assetPrefix: isGithubPages ? '/portfolio_next_styled/' : '',
+    output: 'export',
+    basePath: '/portfolio_next_styled',
+    assetPrefix: '/portfolio_next_styled',
     compiler: {
-        styledComponents: true, // works if .babelrc is removed!
+        styledComponents: true,
     },
 };
-
-export default nextConfig;
-
