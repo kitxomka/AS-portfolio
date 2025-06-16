3384c9d6d0638460dcee31358d46a8bddcf81e54
AlonaCyberAce <alona@cyberace.ca>
2025-06-16 08:51:22 -0600

    some changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio_next_styled',
+  assetPrefix: '/portfolio_next_styled',
+};
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/portfolio_next_styled',
+    assetPrefix: '/portfolio_next_styled',
+    compiler: {
+        styledComponents: true,
+    },
+};
 
 export default nextConfig;
