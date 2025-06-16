c7034552561d6472b2ad0ef08673bcffae02e55d
AlonaCyberAce <alona@cyberace.ca>
2025-06-16 10:13:26 -0600

    some changes

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/portfolio_next_styled',
-  assetPrefix: '/portfolio_next_styled',
-};
-
-export default nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,24 @@
+// const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
+// const nextConfig = {
+//     output: 'export',
+//     basePath: isGithubPages ? '/portfolio_next_styled' : '',
+//     assetPrefix: isGithubPages ? '/portfolio_next_styled/' : '',
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig = {
-    output: 'export',
-    basePath: '/portfolio_next_styled',
-    assetPrefix: '/portfolio_next_styled',
+    output: 'export', // tells Next to prepare for static export
+    basePath: isGithubPages ? '/portfolio_next_styled' : '',
+    assetPrefix: isGithubPages ? '/portfolio_next_styled/' : '',
     compiler: {
-        styledComponents: true,
+        styledComponents: true, // works if .babelrc is removed!
     },
 };
 
 export default nextConfig;
+
